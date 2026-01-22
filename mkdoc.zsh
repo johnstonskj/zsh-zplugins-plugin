0e518222d9e2780eff52fe8e11aa98cf6d587558
Simon Johnston <johnstonskj@gmail.com>
2026-01-22 12:14:46 -0800

    fix: missing '$'

--- mkdoc.zsh
+++ mkdoc.zsh
@@ -8,12 +8,15 @@ fi
 PLUGIN_NAME=zplugins
 INPUT_PATH=${${PWD:-.}:P}
 OUTPUT_PATH=${INPUT_PATH}/doc
-MODULE_PATH={INPUT_PATH}/${PLUGIN_NAME}
+MODULE_PATH=${INPUT_PATH}/${PLUGIN_NAME}
 
+echo -n '.'
 shdoc ${INPUT_PATH}/${PLUGIN_NAME}.plugin.zsh > ${OUTPUT_PATH}/index.md
 
 if [[ -d ${MODULE_PATH} ]]; then
     for file in ${MODULE_PATH}/*.zsh; do
+        echo -n '.'
         shdoc ${file} > ${OUTPUT_PATH}/${${file:t}:r}.md
     done
 fi
+echo ' done'
